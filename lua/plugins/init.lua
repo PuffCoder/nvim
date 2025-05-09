e2c9b8315d8345ccbbdd2a6f5f7d16464f7ae4c6
PUffCoder <heyhyi31@gmail.com>
2025-05-09 19:06:07 +1200

    remove the old mason repo

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,14 +4,12 @@ return {
   { "MunifTanjim/nui.nvim" },
 
   {
-    -- "williamboman/mason.nvim",
     "mason-org/mason.nvim",
     config = function()
       require("mason").setup()
     end,
   },
   {
-    -- "williamboman/mason-lspconfig.nvim",
     "mason-org/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,30 @@
+-- return {
+--   "telescope.nvim",
+--   dependencies = {
+--     "nvim-telescope/telescope-fzf-native.nvim",
+--     build = "make",
+--     config = function()
+--       local telescope = require("telescope")
+--       telescope.load_extension("fzf")
+--     end,
+--     keys = {
+--       {
+--         "<leader>sm",
+--         function()
+--           require("telescope.builtin").marks({ hidden = true })
+--         end,
+--       },
+--     },
+--   },
+-- }
+
 return {
-  "telescope.nvim",
-  dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    config = function()
-      local telescope = require("telescope")
-      telescope.load_extension("fzf")
-    end,
-    keys = {
+  'nvim-telescope/telescope.nvim', tag = '0.1.5',
+  dependencies = { 'nvim-lua/plenary.nvim'},
+  config = function()
+  local builtin = require("telescope.builtin")
+  end,
+     keys = {
       {
         "<leader>sm",
         function()
@@ -15,5 +32,4 @@ return {
         end,
       },
     },
-  },
 }
